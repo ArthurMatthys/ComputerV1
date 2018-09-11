4064c75d1ddda30252e1094a856208c808b49914
Arthur Matthys <amatthys@z1r9p4.le-101.fr>
2018-09-11 18:52:33 +0200

    Bonne partie du parsing de faite

--- Parsing.py
+++ Parsing.py
@@ -10,38 +10,46 @@ def is_in(lst, a):
     return(0)
 
 
-def is_number(s):
-    p = 0
-    if s == "":
-        print("empty string")
-        return(0)
-    for i in s:
-        if (i == '.'):
-            p += 1
-        elif (i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9'):
-            continue
-        else:
-            return (-1)
-    return (p)
+def is_float(s):
+    try:
+        a = float(s)
+    except ValueError:
+        return False
+    else:
+        return True
 
-def atof(str):
-    i = 0
-    j = 0
-    p = 0
-    for t in str:
-        if (t == '.'):
-            p == 1
-        elif (p):
-            j = j / 10 + int(t) / 10
+def is_int(s):
+    try:
+        a = float(s)
+        b = int (a)
+    except ValueError:
+        return False
+    else:
+        return a == b
+
+def handle_arg(lst):
+    print(lst)
+    if (len(lst) == 1):
+        if is_float(lst[0]):
+            return 1
         else:
-            i = i * 10 + int (t)
-    print("i et j et i + j :")
-    print(i, j, i+j)
-    return (i + j)
+            return 0
+    elif (len(lst) == 2 and (is_float(lst[0]) or is_float(lst[1])) and (is_in(lst[0], 'X') or is_in(lst[1], 'X'))):
+        return 1
+    elif (len(lst) < 1 or len(lst) > 2):
+        return 0
+    return 0
+
+def arg_to_tab(lst, i):
+    if len(lst )
+    return 0
+
 
 def handle_input(lst):
     lst = lst.replace(" ", "")
+    lst = lst.replace("x", "X")
     lst = lst.split("=")
+    t = []
     if (len(lst) <= 1):
         print(" \'=\' is needed, would you like to add \" = 0\" ? (y) or (n)")
         while (1):
@@ -56,12 +64,14 @@ def handle_input(lst):
                 print("please, enter y or n")
     for i in range(len(lst)):
         lst[i] = lst[i].split("+")
-        for j in lst[i]:
-            if (is_in(j, 'X')):
-                j = j.replace("X", "")
-                j = j.split("^")
-    mult = 0
-    is_x = 0
-    print(lst)
+        for j in range(len(lst[i])):
+            lst[i][j] = lst[i][j].split("*")
+            if handle_arg(lst[i][j]) == 0:
+                print("wront input")
+                exit()
+            #else:
+            #    t += arg_to_tab(lst[i][j])
+
+    print(t)
 
 handle_input(a)
